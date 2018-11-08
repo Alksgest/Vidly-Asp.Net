879bae264a90a36090cf20ac4b63d546ee45d8b2
Vladyslav <Alksgest@gmail.com>
2018-11-08 10:15:02 +0200

    added migrations, changed theme

--- VidlyApp/Controllers/CustomersController.cs
+++ VidlyApp/Controllers/CustomersController.cs
@@ -5,42 +5,39 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using VidlyApp.Models;
+using System.Data.Entity;
+
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        List<Customer> CustomersList = new List<Customer>
-            {
-                new Customer{Name = "John Smith", Id = 0 },
-                new Customer{Name = "Mary Williams", Id = 1 },
-                new Customer{Name = "Vladyslav Kyrychenko", Id = 2 },
-                new Customer{Name = "Vlad Noda", Id = 3 }
-            };
+        private List<Customer> CustomersList;
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+            CustomersList = _context.Customers.Include(c => c.MembershipType).ToList();
+        }
 
-        List<Movie> MoviesList = new List<Movie>
-            {
-                new Movie{Name = "Robin Hood", Id = 0 },
-                new Movie{Name = "Creed II", Id = 1},
-                new Movie{Name = "Disobedience", Id = 2 },
-                new Movie{Name = "Coco", Id = 3 }
-            };
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         [Route("Customers/{id:int}")]
         public ActionResult Index(int? Id)
         {
-            if (!Id.HasValue)
-                Id = 1;
+            var customer = CustomersList.SingleOrDefault(c => c.Id == Id);
 
-            var viewModel = CustomersList[Id.Value - 1];
-
-            return View(viewModel);
+            return View(customer);
         }
         [Route("Customers")]
         public ActionResult Customers()
         {
             var viewModel = new CustomersViewModel()
             {
-                Customers = CustomersList
+                Customers = this.CustomersList
             };
             return View(viewModel);
         }
